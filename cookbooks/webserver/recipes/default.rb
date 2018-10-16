3cb0c77ec5dcad73109e67be82a65aa679415988
Sharique masood <43162614+shariquemasood@users.noreply.github.com>
2018-10-16 16:47:26 +0530

    Update default.rb

--- cookbooks/webserver/recipes/default.rb
+++ cookbooks/webserver/recipes/default.rb
@@ -7,12 +7,9 @@
 case node['platform']
 when 'centos'
   # Find Red Hat release version
-
-
   # Call implementation recipe based on platform
   include_recipe 'webserver::centos'
-
-
+  
 when 'ubuntu'
   # Call Ubuntu platform recipe
   include_recipe 'webserver::ubuntu'
